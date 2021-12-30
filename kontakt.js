1bde7f85618183c43f29c8d08cc4246bc00258f4
Kasztan404 <m.pestka@protonmail.com>
2021-12-30 22:49:58 +0100

    New page Kontakt! with my hidden email :)

---
+++ kontakt.js
@@ -0,0 +1,35 @@
+	var t="tonmej";
+	var wynik=0;
+function pokazMail() {
+	var paragraf = document.createElement("p");  
+	var textparagraf = document.createTextNode("Mój mejl to: "+"m.pes"+t[0]+"ka"+"@pro"+t+"l"+".com");
+	paragraf.appendChild(textparagraf);    
+	document.getElementsByClassName('tresc2')[0].appendChild(paragraf);
+}
+function input() {
+	var odpowiedz = document.getElementById("rozwiazanie").value;
+	liczba = parseInt(odpowiedz);
+	if (wynik==0){
+		if (odpowiedz =="" || odpowiedz==" ") {
+			alert("Hola! Brak odpowiedzi!");
+			document.getElementById("rozwiazanie").value="";
+		}
+		else if (isNaN(liczba)) {
+			alert("Wprowadź jakąś liczbę!");
+			document.getElementById("rozwiazanie").value="";
+		}	
+		else {
+			if ( liczba==3)
+			{
+				alert ("TAK!");
+				pokazMail();
+				wynik=1;
+			}
+			else {
+				alert ("To nie ta liczba!");
+				document.getElementById("rozwiazanie").value="";
+			}
+		
+		}
+	}
+}
